efbecf31657992569dbf02723582749fc62b0398
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-09-04 08:08:21 +0000

    segmentation done

--- source/features/segmentation.blade.php
+++ source/features/segmentation.blade.php
@@ -2,6 +2,21 @@
 
 @section('body')
 
-Email Marketing
+@include('_partials.features.hero',[
+  'sub_header' => 'Mautic Segmentation', 
+  'header' => 'Connect with the right people', 
+  'desc' => 'Mautic\'s segmentation empowers precise audience targeting, enhancing campaign effectiveness without the need for coding or technical expertise.',
+  'image' => '/assets/images/features/segmentation.jpeg',
+  'alt' => 'Mautic segmentation.'
+])
+
+@include('_partials.features.in-action', [
+  'image' => '/assets/images/features/segments-builder.png',
+  'alt' => 'Mautic Segments in action.'
+])
+  
+@include('_partials.features.features-blocks')
+@include('_layouts.cta')  
+
 
 @endsection
