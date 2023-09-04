4711df2054a12bb627d5c521f15ef3285c3e29f6
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-09-04 09:57:27 +0000

    why page

--- source/products/why-steer-campaign-and-mautic.blade.php
+++ source/products/why-steer-campaign-and-mautic.blade.php
@@ -1,7 +1,249 @@
 @extends('_layouts.main')
 
 @section('body')
+<!-- Hero -->
+<div class="relative overflow-hidden before:absolute before:top-0 before:left-1/2 before:bg-[url('/assets/images/why-steercampaign/bg.svg')] before:bg-no-repeat before:bg-top before:w-full before:h-full before:-z-[1] before:transform before:-translate-x-1/2 dark:before:bg-[url('/assets/images/why-steercampaign/bg-dark.svg')]">
+    <div class="max-w-[85rem] mx-auto px-2 sm:px-4 lg:px-6 pt-24 pb-10">
+      <!-- Announcement Banner -->
+      <div class="flex justify-center">
+        <h3 class="block font-bold text-gray-800 text-xl md:text-2xl lg:text-3xl dark:text-gray-200">
+            Why Steer Campaign?
+        </h3>
+  
+        <a class="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-xs text-gray-600 p-2 px-3 rounded-full transition hover:border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-400" href="#">
+            
+        </a>
+      </div>
+      <!-- End Announcement Banner -->
+  
+      <!-- Title -->
+      <div class="mt-5 max-w-4xl text-center mx-auto">
+        <h1 class="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
+            We are the Developers Behind Key Components of Mautic!
+        </h1>
+      </div>
+      <!-- End Title -->
+  
+      <div class="mt-5 max-w-3xl text-center mx-auto">
+        <p class="text-lg text-gray-600 dark:text-gray-400">Steer Campaign: A Pivotal and Constant Contributor to Mautic's Essential Development!</p>
+      </div>
+    </div>
+  </div>
+  <!-- End Hero -->
 
-Email Marketing
+<!-- Testimonials -->
+<div class="overflow-hidden bg-slate-800">
+    <div class="relative max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <!-- Title -->
+      <div class="max-w-4xl w-3/4 lg:w-1/2 mb-6 sm:mb-10 md:mb-16">
+        <h2 class="text-2xl sm:text-3xl lg:text-4xl text-white font-semibold">
+          Loved by business and individuals across the globe
+        </h2>
+      </div>
+      <!-- End Title -->
+  
+      <!-- Grid -->
+      <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <!-- Card -->
+        <div class="flex h-auto">
+          <div class="flex flex-col bg-white rounded-xl dark:bg-slate-900">
+            <div class="flex-auto p-4 md:p-6">
+              <p class="text-base italic md:text-lg text-gray-800 dark:text-gray-200">
+                "If you have Mautic trouble, this is the company to go to. Excellent work, quick and great communications."
+              </p>
+            </div>
+  
+            <div class="p-4 bg-gray-100 rounded-b-xl md:px-7 dark:bg-slate-700">
+              <div class="flex items-center">
+                <div class="flex-shrink-0">
+                  <img class="h-8 w-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]" src="/assets/images/why-steercampaign/avatar.png" alt="Customers love Steer Campaign">
+                </div>
+  
+                <div class="grow ml-3">
+                  <p class="text-sm sm:text-base font-semibold text-gray-800 dark:text-gray-200">
+                    Jari
+                  </p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">
+                    Marketing Director | Finland
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Card -->
+  
+        <!-- Card -->
+        <div class="flex h-auto">
+          <div class="flex flex-col bg-white rounded-xl dark:bg-slate-900">
+            <div class="flex-auto p-4 md:p-6">
+              <p class="text-base italic md:text-lg text-gray-800 dark:text-gray-200">
+                " After dealing with a couple of nonsense Mautic "professionals", Steer Campaign was a true light at the end of the tunnel. They got us up in running in a week, after spending over a month failing.
+                Highly recommended. "
+              </p>
+            </div>
+  
+            <div class="p-4 bg-gray-100 rounded-b-xl md:px-7 dark:bg-slate-700">
+              <div class="flex items-center">
+                <div class="flex-shrink-0">
+                    <img class="h-8 w-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]" src="/assets/images/why-steercampaign/avatar1.png" alt="Customers love Steer Campaign">
+                </div>
+  
+                <div class="grow ml-3">
+                  <p class="text-sm sm:text-base font-semibold text-gray-800 dark:text-gray-200">
+                    Shondra
+                  </p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">
+                    Senior Vice President | USA
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Card -->
+  
+        <!-- Card -->
+        <div class="flex h-auto">
+          <div class="flex flex-col bg-white rounded-xl dark:bg-slate-900">
+            <div class="flex-auto p-4 md:p-6">
+              <p class="text-base italic md:text-lg text-gray-800 dark:text-gray-200">
+                " Steer Campaign is an outstanding Mautic-focused company offering expert guidance on best practices and resolving technical challenges swiftly. They efficiently addressed our Mautic technical issues. If you're seeking assistance with Mautic, I wholeheartedly recommend Steer Campaign. "
+              </p>
+            </div>
+  
+            <div class="p-4 bg-gray-100 rounded-b-xl md:px-7 dark:bg-slate-700">
+              <div class="flex items-center">
+                <div class="flex-shrink-0">
+                    <img class="h-8 w-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]" src="/assets/images/why-steercampaign/avatar1.png" alt="Customers love Steer Campaign">
+                </div>
+  
+                <div class="grow ml-3">
+                  <p class="text-sm sm:text-base font-semibold text-gray-800 dark:text-gray-200">
+                    Shawngela
+                  </p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">
+                    Entrepreneur | Happy customer
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Card -->
+      </div>
+      <!-- End Grid -->
+  
+      <!-- Grid -->
+      <div class="mt-20 grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8">
+        <!-- Stats -->
+        <div>
+          <h4 class="text-lg sm:text-xl font-semibold text-white">Customers Served</h4>
+          <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-500">100+</p>
+          <p class="mt-1 text-gray-400">hosting, training, support</p>
+        </div>
+        <!-- End Stats -->
+  
+        <!-- Stats -->
+        <div>
+          <h4 class="text-lg sm:text-xl font-semibold text-white">Returning customers</h4>
+          <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-500">88%</p>
+          <p class="mt-1 text-gray-400">work again with us</p>
+        </div>
+        <!-- End Stats -->
+  
+        <!-- Stats -->
+        <div>
+          <h4 class="text-lg sm:text-xl font-semibold text-white">Customer Referrals</h4>
+          <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-500">100%</p>
+          <p class="mt-1 text-gray-400">recommend us</p>
+        </div>
+        <!-- End Stats -->
+      </div>
+      <!-- End Grid -->
+  
+      <!-- SVG Element -->
+      <div class="absolute bottom-0 right-0 transform lg:translate-x-32" aria-hidden="true">
+        <svg class="w-40 h-auto sm:w-72" width="1115" height="636" viewBox="0 0 1115 636" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.990203 279.321C-1.11035 287.334 3.68307 295.534 11.6966 297.634L142.285 331.865C150.298 333.965 158.497 329.172 160.598 321.158C162.699 313.145 157.905 304.946 149.892 302.845L33.8132 272.418L64.2403 156.339C66.3409 148.326 61.5475 140.127 53.5339 138.026C45.5204 135.926 37.3213 140.719 35.2207 148.733L0.990203 279.321ZM424.31 252.289C431.581 256.26 440.694 253.585 444.664 246.314C448.635 239.044 445.961 229.931 438.69 225.96L424.31 252.289ZM23.0706 296.074C72.7581 267.025 123.056 230.059 187.043 212.864C249.583 196.057 325.63 198.393 424.31 252.289L438.69 225.96C333.77 168.656 249.817 164.929 179.257 183.892C110.144 202.465 54.2419 243.099 7.92943 270.175L23.0706 296.074Z" fill="currentColor" class="fill-orange-500"/>
+          <path d="M451.609 382.417C446.219 388.708 446.95 398.178 453.241 403.567L555.763 491.398C562.054 496.788 571.524 496.057 576.913 489.766C582.303 483.474 581.572 474.005 575.281 468.615L484.15 390.544L562.222 299.413C567.612 293.122 566.881 283.652 560.59 278.263C554.299 272.873 544.829 273.604 539.44 279.895L451.609 382.417ZM837.202 559.655C841.706 566.608 850.994 568.593 857.947 564.09C864.9 559.586 866.885 550.298 862.381 543.345L837.202 559.655ZM464.154 407.131C508.387 403.718 570.802 395.25 638.136 410.928C704.591 426.401 776.318 465.66 837.202 559.655L862.381 543.345C797.144 442.631 718.724 398.89 644.939 381.709C572.033 364.734 504.114 373.958 461.846 377.22L464.154 407.131Z" fill="currentColor" class="fill-cyan-500"/>
+          <path d="M447.448 0.194357C439.203 -0.605554 431.87 5.43034 431.07 13.6759L418.035 148.045C417.235 156.291 423.271 163.623 431.516 164.423C439.762 165.223 447.095 159.187 447.895 150.942L459.482 31.5025L578.921 43.0895C587.166 43.8894 594.499 37.8535 595.299 29.6079C596.099 21.3624 590.063 14.0296 581.818 13.2297L447.448 0.194357ZM1086.03 431.727C1089.68 439.166 1098.66 442.239 1106.1 438.593C1113.54 434.946 1116.62 425.96 1112.97 418.521L1086.03 431.727ZM434.419 24.6572C449.463 42.934 474.586 81.0463 521.375 116.908C568.556 153.07 637.546 187.063 742.018 200.993L745.982 171.256C646.454 157.985 582.444 125.917 539.625 93.0974C496.414 59.978 474.537 26.1903 457.581 5.59138L434.419 24.6572ZM742.018 200.993C939.862 227.372 1054.15 366.703 1086.03 431.727L1112.97 418.521C1077.85 346.879 956.138 199.277 745.982 171.256L742.018 200.993Z" fill="currentColor" class="fill-white"/>
+        </svg>
+      </div>
+      <!-- End SVG Element -->
+    </div>
+  </div>
+  <!-- End Testimonials -->  
 
+<!-- Icon Blocks -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <div class="max-w-2xl mx-auto">
+      <!-- Grid -->
+      <div class="grid gap-12">
+        <div>
+          <h2 class="text-3xl text-gray-800 font-bold lg:text-4xl dark:text-white">
+            Steer Campaign is your choice for Mautic
+          </h2>
+          <p class="mt-3 text-gray-800 dark:text-gray-400">
+            In the dynamic world of marketing automation, finding the right partner to navigate the Mautic landscape can be a game-changer for your business. Steer Campaign stands out as the best provider for Mautic, and here's why:
+          </p>
+        </div>
+
+        <div class="space-y-6 lg:space-y-10">
+          <!-- Icon Block -->
+          <div class="flex">
+            <svg class="flex-shrink-0 mt-2 h-6 w-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+              <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+            </svg>
+            <div class="ml-5 sm:ml-8">
+              <h3 class="text-base sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+                Unmatched Expertise
+              </h3>
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                Steer Campaign boasts a team of seasoned experts with deep knowledge and experience in Mautic. Our professionals have honed their skills through years of hands-on experience, making them adept at addressing complex Mautic challenges and optimizing your marketing automation processes.
+              </p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+
+          <!-- Icon Block -->
+          <div class="flex">
+            <svg class="flex-shrink-0 mt-2 h-6 w-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+            </svg>
+            <div class="ml-5 sm:ml-8">
+              <h3 class="text-base sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+                Proven Track Record
+              </h3>
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                Our track record speaks for itself. Steer Campaign has consistently delivered exceptional results for clients across various industries. From small businesses to large enterprises, our Mautic solutions have empowered organizations to achieve their marketing goals efficiently and effectively.
+              </p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+
+          <!-- Icon Block -->
+          <div class="flex">
+            <svg class="flex-shrink-0 mt-2 h-6 w-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+            </svg>
+            <div class="ml-5 sm:ml-8">
+              <h3 class="text-base sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+                Tailored Solutions
+              </h3>
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                We understand that every business is unique, and so are its marketing automation needs. Steer Campaign takes a personalized approach to develop tailored Mautic solutions that align with your specific objectives. Our solutions are not one-size-fits-all; they are designed to fit your business like a glove.
+              </p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+      </div>
+      <!-- End Grid -->
+    </div>
+  </div>
+  <!-- End Icon Blocks -->  
+
+  @include('_layouts.cta')    
 @endsection
